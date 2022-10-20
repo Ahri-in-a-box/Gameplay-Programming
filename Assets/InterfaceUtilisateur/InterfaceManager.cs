5ff21e8135ecd61ae5bc96022f22ae2c92cade65
Eléonore Vaissaire <76736017+ElVaissaire@users.noreply.github.com>
2022-10-20 15:34:47 +0200

    UI
    Création des prototype d'UI
    Lien entre les UI
    Suppression du pack des produits inutilisables

---
+++ Assets/InterfaceUtilisateur/InterfaceManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InterfaceManager : MonoBehaviour
+{
+    [SerializeField] private GameObject m_PanelAccueil;
+    [SerializeField] private GameObject m_PanelCredits;
+    [SerializeField] private GameObject m_PanelMenu;
+    [SerializeField] private GameObject m_PanelControles;
+    [SerializeField] private GameObject m_PanelFin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PanelAccueil.SetActive(true);
+        m_PanelCredits.SetActive(false);
+        m_PanelMenu.SetActive(false);
+        m_PanelControles.SetActive(false);
+        m_PanelFin.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        m_PanelAccueil.SetActive(false);
+        m_PanelMenu.SetActive(true);
+        m_PanelControles.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowControls()
+    {
+        m_PanelControles.SetActive(true);
+    }
+
+    public void QuitControls()
+    {
+        m_PanelControles.SetActive(false);
+    }
+    
+    public void ShowCredit()
+    {
+        m_PanelAccueil.SetActive(false);
+        m_PanelCredits.SetActive(true);
+    }
+
+    public void QuitCredit()
+    {
+        m_PanelCredits.SetActive(false);
+        m_PanelAccueil.SetActive(true);
+    }
+
+    public void ReturnAccueil()
+    {
+        m_PanelAccueil.SetActive(true);
+        m_PanelCredits.SetActive(false);
+        m_PanelMenu.SetActive(false);
+        m_PanelControles.SetActive(false);
+        m_PanelFin.SetActive(false);
+    }
+
+}
